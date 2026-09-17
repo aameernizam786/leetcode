4e03ad33b047cecb384a6e2923a4b150aa4b894a
aameernizam786 <aameernizam0001201@gmail.com>
2026-09-17 13:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (47.56%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+  public int uniquePaths(int m, int n) {
+   int t_way=m+n-2;
+   int k=Math.min(m-1,n-1);
+   long result=1;
+   for(int i=1 ;i<=k ; i++){
+    result=result*(t_way-k+i)/i;
+   }
+   return (int) result;
+    }
+}
